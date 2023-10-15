7c6b9cfba8a1b35ba27e056f9340ec928ad473f9
165111_Cuong <cuongpd129@gmail.com>
2023-10-15 10:37:44 +0700

    chnage lib

--- frontend/pages/question/index.js
+++ frontend/pages/question/index.js
@@ -6,7 +6,7 @@ import {
   Button,
 } from '@material-tailwind/react';
 import classNames from 'classnames';
-import { useRouter } from 'next/navigation';
+import { useRouter } from 'next/router';
 const TABLE_HEAD = ['Câu hỏi', 'Đáp án', 'Employed', ''];
 
 const TABLE_ROWS = [
