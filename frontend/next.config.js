522c2e30bc191f8c22b24c20c6e4f2935ff71dec
165111_Cuong <cuongpd129@gmail.com>
2023-10-21 00:08:52 +0700

    http

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,23 +1,4 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
-
-module.exports = (phase, { defaultConfig }) => {
-  return {
-    ...defaultConfig,
-    reactStrictMode: true,
-
-    webpack: (config) => {
-      config.resolve = {
-        ...config.resolve,
-        fallback: {
-          fs: false,
-          path: false,
-          os: false,
-        },
-      };
-      return config;
-    },
-  };
+const nextConfig = {
+  reactStrictMode: true,
 };
